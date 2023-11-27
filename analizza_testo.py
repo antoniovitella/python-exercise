fa16c13e23bf9ba73cc98dba6fcf96bc456b0199
antoniovitella <150900944+antoniovitella@users.noreply.github.com>
2023-11-27 21:42:59 +0100

    Analizza un testo e restituisce un dizionario con numero e ripetizioni delle parole e lunghezza media delle parole rispetto al testo
    Il codice fornito definisce una funzione analizza_testo che accetta un testo come input e restituisce un dizionario contenente alcune analisi sul testo fornito.
    
    Ecco una spiegazione passo per passo del codice:
    
    1.parole = testo.lower().split(): Prende il testo in input e lo trasforma completamente in lettere minuscole con .lower(), quindi lo suddivide in parole utilizzando .split(). Questo crea una lista chiamata parole contenente tutte le parole presenti nel testo fornito, ignorando la punteggiatura e distinguendo le parole in base agli spazi.
    
    2.numero_parole = len(parole): Calcola il numero totale di parole nel testo contando quanti elementi ci sono nella lista parole.
    
    3.frequenza_parole = {}: Inizializza un dizionario vuoto chiamato frequenza_parole che verrà utilizzato per tenere traccia della frequenza di ogni parola nel testo.
    
    4.lunghezza_totale = 0: Inizializza una variabile lunghezza_totale a zero, che verrà utilizzata per calcolare la lunghezza totale di tutte le parole nel testo.
    
    5.Il ciclo for parola in parole: itera attraverso ogni parola nella lista parole. Per ogni parola, controlla se è già presente nel dizionario frequenza_parole. Se è presente, incrementa il conteggio di quella parola. Se non è presente, aggiunge la parola al dizionario e inizializza il conteggio a 1.
    
    6.lunghezza_media = round(lunghezza_totale / numero_parole, 3) if numero_parole > 0 else 0.0: Calcola la lunghezza media delle parole nel testo. Se il numero totale di parole è maggiore di zero, calcola la media della lunghezza totale delle parole diviso per il numero di parole e arrotonda il risultato a 3 decimali. Se il numero totale di parole è zero, restituisce 0.0 per evitare divisioni per zero.
    
    7.risultato = {...}: Costruisce un dizionario chiamato risultato che contiene le informazioni richieste. Questo dizionario include il numero totale di parole, un conteggio della frequenza di ogni parola nel testo e la lunghezza media delle parole.
    
    8.Infine, la funzione restituisce il dizionario risultato.
    
    9.All'esterno della funzione, il codice richiede all'utente di inserire del testo, quindi chiama la funzione analizza_testo con il testo inserito e stampa il dizionario risultante.

--- analizza_testo.py
+++ analizza_testo.py
@@ -1,21 +1,21 @@
 def analizza_testo(testo):
-    parole=testo.lower().split() #Converte il tyesto in minuscole e separa le parole
-    numero_parole=len(parole) #Numero totale di parole
-    frequenza_parole={}#Dizionario per tenere gtraccia della frequenza delle parole
-    lunghezza_totale=0 #Inizializza la lunghezza totale delle parole
+    parole=testo.lower().split() # Converte il testo in minuscole e separa le parole
+    numero_parole=len(parole) # Numero totale di parole
+    frequenza_parole={}# Dizionario per tenere traccia della frequenza delle parole
+    lunghezza_totale=0 # Inizializza la lunghezza totale delle parole
     
-    #Calcola la frequenza di ogni parola e la lunghezza totale
+    # Calcola la frequenza di ogni parola e la lunghezza totale
     for parola in parole:
         if parola in frequenza_parole:
             frequenza_parole[parola] += 1
         else:
             frequenza_parole[parola] = 1
-    #Calcola la lunghezza medi delle parole
+    # Calcola la lunghezza media delle parole
     lunghezza_media = round(lunghezza_totale / numero_parole, 3) if numero_parole>0 else 0.0
     
-    #Costruisci il dizionario con le informazioni richieste
+    # Costruisci il dizionario con le informazioni richieste
     risultato = {
-        'numero_pagine': numero_parole,
+        'numero_parole': numero_parole,
         'parole': frequenza_parole,
         'lunghezza_media': lunghezza_media
     }
