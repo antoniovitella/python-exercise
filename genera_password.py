6f4b140abc0b41f05d7390dd676c6bcc435745c0
antoniovitella <150900944+antoniovitella@users.noreply.github.com>
2023-11-27 22:09:46 +0100

    Generatore di password causale di lunghezza specificata
    Questo programma Python genera una password casuale di lunghezza specificata.
    Ecco come funziona:
    
    1.Import delle librerie necessarie:
    Importiamo due librerie:
    
    random: per generare numeri casuali e scegliere elementi casuali da una sequenza.
    
    string: fornisce sequenze di caratteri ASCII come lettere minuscole e maiuscole, cifre e caratteri speciali.
    
    2.Definizione della funzione genera_password:
    
    Definiamo una funzione chiamata genera_password che prende un parametro lunghezza, che rappresenta la lunghezza desiderata della password.
    
    3.Creazione dei caratteri possibili:
    Creiamo una variabile caratteri che contiene tutti i caratteri che vogliamo includere nella password: lettere minuscole, lettere maiuscole, cifre e caratteri speciali.
    
    4.Generazione della password:
    Utilizziamo random.choice() per scegliere casualmente caratteri dalla sequenza caratteri per la lunghezza specificata.
    
    5.Ritorno della password generata:
    La funzione restituisce la password generata.
    
    6.Utilizzo della funzione per generare una password:
    Usiamo la funzione genera_password per generare una password di lunghezza specificata. Stampiamo la password generata sulla console.
    
    Questo programma può essere personalizzato modificando la lunghezza della password desiderata e può essere adattato per includere ulteriori requisiti specifici per la password se necessario!

---
+++ genera_password.py
@@ -0,0 +1,9 @@
+import random
+import string
+def genera_password(lunghezza):
+    caratteri = string.ascii_letters + string.digits + string.punctuation
+    password =''.join(random.choice (caratteri) for _ in range(lunghezza))
+    return password
+lunghezza_password = 8 # Puoi cambiare la lunghezza della password qui
+nuova_password = genera_password(lunghezza_password)
+print("La tua nuova password casuale è:",nuova_password)
