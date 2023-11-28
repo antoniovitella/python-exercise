31be9e635f83887eac37128e1a91024f5479bf7c
antoniovitella <150900944+antoniovitella@users.noreply.github.com>
2023-11-28 17:13:18 +0100

    Update genera_password.py

--- genera_password.py
+++ genera_password.py
@@ -1,9 +1,9 @@
 import random
 import string
 def genera_password(lunghezza):
-    caratteri = string.ascii_letters + string.digits + string.punctuation
+    caratteri = string.ascii_letters + string.digits
     password =''.join(random.choice (caratteri) for _ in range(lunghezza))
     return password
-lunghezza_password = 8 # Puoi cambiare la lunghezza della password qui
+lunghezza_password = 12 # Puoi cambiare la lunghezza della password qui
 nuova_password = genera_password(lunghezza_password)
 print("La tua nuova password casuale è:",nuova_password)
